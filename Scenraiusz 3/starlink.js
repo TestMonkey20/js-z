99b523bb9b08a9e7e1feb224b45604a5639dc9f3
TestMonkey20 <aleksander.grodecki@gmail.com>
2021-07-16 01:22:20 +0200

    Display + display logic

---
+++ Scenraiusz 3/starlink.js
@@ -0,0 +1,43 @@
+(async () => {
+	try {
+		const items = await fetchStarlink()
+		const params = new URLSearchParams(document.location.search)
+		const curPage = Number.parseInt(params.get('p')) || 0
+		const pageSize = 10
+		const maxPagination = 5
+		const pageCount = Math.ceil(items.length/pageSize)
+
+		const table = document.querySelector('table.starlink-table > tbody')
+		table.innerHTML = items
+			.map((it, n) => ({n: n+1,...it}))
+			.slice(pageSize*curPage, pageSize*(curPage+1))
+			.map(it => `
+				<tr>
+					<td>${it.n}</td>
+					<td>${it.id}</td>
+					<td>${it.name}</td>
+					<td>${it.launchDate}</td>
+				</tr>
+			`)
+			.join('')
+
+		const pagination = document.querySelector('footer > .pagination')	
+		pagination.innerHTML = [...Array(maxPagination*2 + 1).keys()]
+			.map(x => curPage - maxPagination + x)
+			.filter(x => x >= 0 && x < pageCount)
+			.map(pageNo => `
+				<a 
+					${ curPage === pageNo ? `class="selected"` : ''} 
+					href="?p=${pageNo}"
+				>
+					${pageNo + 1}
+				</a>
+			`)
+			.join('')
+		
+	}
+	catch(err) {
+		throw err
+	}
+})()
+
